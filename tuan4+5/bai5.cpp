86997839f85a95e2032ccdc6dfc21510e4808204
VanVu1306 <159514761+VanVu1306@users.noreply.github.com>
2024-04-15 19:36:05 +0700

    Add files via upload

---
+++ tuan4+5/bai5.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 10;
+
+int main() {
+    char str[N];
+    cin >> str;
+    cout << "_" << str << "_";
+    cout << endl;
+
+    //nhỏ hơn N
+    cin >> str;
+    cout << "_" << str << "_";
+    cout << endl;
+
+    //bằng N
+    cin >> str;
+    cout << "_" << str << "_";
+    cout << endl;
+
+    //lớn hơn N
+    cin >> str;
+    cout << "_" << str << "_";
+    cout << endl;
+
+    return 0;
+}
